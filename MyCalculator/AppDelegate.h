5ab3a814ded86a3234a096310eacc541af736a32
Miguel S. Mendoza <miguelsmendoza@server.villodre.private>
2014-11-30 16:48:01 +0100

    Initial Commit

---
+++ MyCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCalculator
+//
+//  Created by Miguel S. Mendoza on 30/11/14.
+//  Copyright (c) 2014 NetRunners. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
